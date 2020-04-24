ec9c705d23c8d897d36d2384c2c2e9b82b702706
math231k <54638811+math231k@users.noreply.github.com>
2020-04-24 09:42:41 +0200

    Arbejder på Project Management klassen

--- src/timetrackingexam/gui/controller/ProjectsOverviewController.java
+++ src/timetrackingexam/gui/controller/ProjectsOverviewController.java
@@ -60,6 +60,7 @@ public class ProjectsOverviewController implements Initializable {
     private void setItemsOnList(ActionEvent event) {
         selectedProject = cbbProjectSelect.getSelectionModel().getSelectedItem();
         lstTaskList.setItems(selectedProject.getTasks());
+        am.setCurrentProject(selectedProject);
     }
     
     private ObservableList<Project> getAllProjects(){
