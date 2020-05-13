5701d1aa9086617090e854c5901eae2d01dafa3f
Jonas Spatzek <Jonas_spatzek@hotmail.com>
2020-05-13 09:31:34 +0200

    Dbdao 2.0

--- src/timetrackingexam/dal/dao/ClientDBDAO.java
+++ src/timetrackingexam/dal/dao/ClientDBDAO.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import timetrackingexam.dal.dbaccess.DBSettings;
 /**
  *
  * @author jonas
