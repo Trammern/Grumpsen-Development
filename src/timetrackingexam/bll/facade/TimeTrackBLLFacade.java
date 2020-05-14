d8a004f847193cd1f93492589177e0bd1999b7bb
rizvan99 <rizvan2312@hotmail.com>
2020-05-14 19:19:38 +0200

    facade

--- src/timetrackingexam/bll/facade/TimeTrackBLLFacade.java
+++ src/timetrackingexam/bll/facade/TimeTrackBLLFacade.java
@@ -5,11 +5,39 @@
  */
 package timetrackingexam.bll.facade;
 
+import java.util.List;
+import timetrackingexam.be.Project;
+import timetrackingexam.be.Task;
+
 /**
  *
  * @author Rizvan
  */
-public class TimeTrackBLLFacade
+public class TimeTrackBLLFacade implements ITimeTrackBLL
 {
+
+    @Override
+    public boolean createTask(Task t, Project p)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteTask(Task selectedTask, Project currentProject)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateTask(Task updateTask)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Task> readTask()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
