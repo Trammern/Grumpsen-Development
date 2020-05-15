5fbf2915b42519e9876a1cbd4753925a7b0d9b20
math231k <54638811+math231k@users.noreply.github.com>
2020-05-15 12:12:10 +0200

    TaskDAO

--- src/timetrackingexam/dal/database/controller/TaskDaoController.java
+++ src/timetrackingexam/dal/database/controller/TaskDaoController.java
@@ -5,11 +5,73 @@
  */
 package timetrackingexam.dal.database.controller;
 
+import java.sql.Connection;
+import javafx.collections.ObservableList;
+import timetrackingexam.be.Task;
+import timetrackingexam.be.TaskTime;
+import timetrackingexam.dal.database.dao.TaskDBDAO;
+import timetrackingexam.dal.database.dbaccess.ConnectionPool;
+
 /**
  *
  * @author Rizvan
  */
 public class TaskDaoController
 {
+    private final ConnectionPool conPool;
+    private final TaskDBDAO taskDAO;
+
+    public TaskDaoController(ConnectionPool conPool)
+    {
+        this.conPool = conPool;
+        taskDAO = TaskDBDAO.getInstance();
+    }
     
+    public Task getSpecificTask(Task t)
+    {
+        Connection con = conPool.checkOut();
+        Task selectetTask = taskDAO.getSpecificTask(con, t);
+        conPool.checkIn(con);
+        return selectetTask;
+    }
+    
+    public ObservableList<Task> getAllTasks()
+    {
+        Connection con = conPool.checkOut();
+        ObservableList<Task> tasks = taskDAO.getAllTasks(con);
+        conPool.checkIn(con);
+        return tasks;
+    }
+
+    public boolean CreateTask(Task task)
+    {
+        Connection con = conPool.checkOut();
+        Boolean create = taskDAO.createTask(con, task);
+        conPool.checkIn(con);
+        return create;
+    }
+    
+    public boolean deleteTask(Task task)
+    {
+        Connection con = conPool.checkOut();
+        boolean deleted = taskDAO.deleteTask(con, task);
+        conPool.checkIn(con);
+        return deleted;
+    }
+    
+    public boolean updateTask(Task task)
+    {
+        Connection con = conPool.checkOut();
+        boolean updated = taskDAO.updateTask(con, task);
+        conPool.checkIn(con);
+        return updated;
+    }
+    
+    ObservableList<TaskTime> getTaskTime(Task task)
+    {
+        Connection con = conPool.checkOut();
+        ObservableList<TaskTime> taskTimes = taskDAO.getTaskTime(con, task);
+        conPool.checkIn(con);
+        return taskTimes;
+    }
 }
--- src/timetrackingexam/dal/database/dao/TaskDBDAO.java
+++ src/timetrackingexam/dal/database/dao/TaskDBDAO.java
@@ -24,60 +24,57 @@ import timetrackingexam.dal.facade.ITaskDal;
  *
  * @author math2
  */
-public class TaskDBDAO implements ITaskDal{
+public class TaskDBDAO{
 
-    private DBSettings dbConnection;
+    private ConnectionPool pool;
     private static TaskDBDAO INSTANCE;
-    
-    private TaskDBDAO()
-    {
-        dbConnection = new DBSettings();
+
+    private TaskDBDAO() {
+        pool = ConnectionPool.getInstance();
     }
-    
-    public static TaskDBDAO getInstance (){
-        if(INSTANCE == null){
+
+    public static TaskDBDAO getInstance() {
+        if (INSTANCE == null) {
             INSTANCE = new TaskDBDAO();
-        }
-        else{
+        } else {
             return INSTANCE;
         }
         return null;
     }
-        
-    @Override
-    public Task getSpecificTask(Task task) {
-        try (Connection con = dbConnection.getConnection()){
+
+    public Task getSpecificTask(Connection con, Task task) {
+        try{
             String sql = "SELECT * FROM task WHERE task.userId = ? AND task.projectId = ?";
             PreparedStatement pstmt = con.prepareStatement(sql);
-            
+
             pstmt.setInt(1, task.getUserId());
             pstmt.setInt(2, task.getProjectId());
-            
+
             ResultSet rs = pstmt.executeQuery();
-            
-            return new Task(rs.getInt("ID"),
+
+            return new Task(
+                    rs.getInt("ID"),
                     rs.getInt("ProjectID"),
                     rs.getInt("UserID"),
                     rs.getString("name")
             );
-                    
+
         } catch (SQLException sqlE) {
             System.out.println("Failed to grab element from database");
             return null;
         }
     }
 
-    @Override
-    public ObservableList<Task> getAllTasks() {
-    try (Connection con = dbConnection.getConnection()){
-            
+    public ObservableList<Task> getAllTasks(Connection con) {
+        try{
+
             ObservableList<Task> tasks = FXCollections.observableArrayList();
-        
+
             String sql = "SELECT * FROM task";
             PreparedStatement pstmt = con.prepareStatement(sql);
-            
+
             ResultSet rs = pstmt.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 tasks.add(new Task(
                         rs.getInt("ID"),
                         rs.getInt("ProjectID"),
@@ -92,112 +89,102 @@ public class TaskDBDAO implements ITaskDal{
         }
     }
 
-    @Override
-    public boolean createTask(Task task) {
-        try(Connection con = dbConnection.getConnection()){
+    public boolean createTask(Connection con, Task task) {
+        try{
             String sql = "INSERT INTO task (ProjectID, Name, Description, userID, TimeAssigned)"
                     + "VALUES"
                     + "(?, ?, ?, ?, ?)";
-            
+
             PreparedStatement pstmt = con.prepareStatement(sql);
-            
+
             pstmt.setInt(1, task.getProjectId());
             pstmt.setString(2, task.getName());
             pstmt.setString(3, task.getDescription());
             pstmt.setInt(4, task.getUserId());
             pstmt.setInt(5, task.getTimeAssigned());
-            
+
             int updatedRows = pstmt.executeUpdate();
-            
+
             return updatedRows > 0;
-        }
-        
-        catch(SQLException sqlE){
+        } catch (SQLException sqlE) {
             System.out.println("Failed to grab element from database");
             Logger.getLogger(TaskDBDAO.class.getName()).log(Level.SEVERE, null, sqlE);
             return false;
         }
     }
 
-    @Override
-    public boolean deleteTask(Task task) {
-        try(Connection con = dbConnection.getConnection()){
+    public boolean deleteTask(Connection con, Task task) {
+        try{
             String sql = "DELETE FROM task"
                     + "WHERE"
                     + "task.ID = ?";
-            
+
             PreparedStatement pstmt = con.prepareStatement(sql);
-            
+
             pstmt.setInt(1, task.getId());
-            
+
             int updatedRows = pstmt.executeUpdate();
-            
+
             return updatedRows > 0;
-        }
-        catch(SQLException sqlE){
+        } catch (SQLException sqlE) {
             System.out.println("Failed to grab element from database");
             Logger.getLogger(TaskDBDAO.class.getName()).log(Level.SEVERE, null, sqlE);
             return false;
         }
     }
 
-    @Override
-    public boolean updateTask(Task task) {
-        try(Connection con = dbConnection.getConnection()){
+    public boolean updateTask(Connection con, Task task) {
+        try{
             String sql = "UPDATE task"
                     + "SET task.Name = ?, task.Description = ?, task.AssignedTime = ?"
                     + "WHERE task.ID = ?";
-            
-            
+
             PreparedStatement pstmt = con.prepareStatement(sql);
-            
+
             pstmt.setString(1, task.getName());
             pstmt.setString(2, task.getDescription());
             pstmt.setInt(3, task.getTimeAssigned());
             pstmt.setInt(4, task.getId());
-            
+
             int updatedRows = pstmt.executeUpdate();
-            
+
             return updatedRows > 0;
-        }
-        catch(SQLException sqlE){
+        } catch (SQLException sqlE) {
             System.out.println("Failed to grab element from database");
             Logger.getLogger(TaskDBDAO.class.getName()).log(Level.SEVERE, null, sqlE);
             return false;
         }
     }
 
-    @Override
-    public ObservableList<TaskTime> getTaskTime(Task task) {
-            try(Connection con = dbConnection.getConnection()){
-                
-                ObservableList<TaskTime> taskTimes = FXCollections.observableArrayList();
-                
-                String sql = "SELECT * FROM Time "
+    public ObservableList<TaskTime> getTaskTime(Connection con, Task task) {
+        try{
+
+            ObservableList<TaskTime> taskTimes = FXCollections.observableArrayList();
+
+            String sql = "SELECT * FROM Time "
                     + "INNER JOIN Task ON Task.ID = Time.TaskID";
-                
-                PreparedStatement pstmt = con.prepareStatement(sql);
-                
-                ResultSet rs = pstmt.executeQuery();
-                
-                while (rs.next()) {                    
-                    taskTimes.add(new TaskTime(
-                            rs.getInt("TaskID"),
-                            rs.getInt("UserID"),
-                            rs.getInt("Sec"),
-                            rs.getInt("Min"),
-                            rs.getInt("Hours"),
-                            LocalDate.parse(rs.getString("Date"))
-                            ));
-                }
-                
-                return taskTimes;
-        }
-        catch(SQLException sqlE){
+
+            PreparedStatement pstmt = con.prepareStatement(sql);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                taskTimes.add(new TaskTime(
+                        rs.getInt("TaskID"),
+                        rs.getInt("UserID"),
+                        rs.getInt("Sec"),
+                        rs.getInt("Min"),
+                        rs.getInt("Hours"),
+                        LocalDate.parse(rs.getString("Date"))
+                ));
+            }
+
+            return taskTimes;
+        } catch (SQLException sqlE) {
             System.out.println("Failed to grab element from database");
             Logger.getLogger(TaskDBDAO.class.getName()).log(Level.SEVERE, null, sqlE);
             return null;
         }
     }
-    
+
 }
