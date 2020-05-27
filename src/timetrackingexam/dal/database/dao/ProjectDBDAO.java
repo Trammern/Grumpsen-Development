532c6c0f14201ac2c760a0049ba23fdd4430180f
Trammern <55829858+Trammern@users.noreply.github.com>
2020-05-27 09:19:07 +0200

    Uhhhh

--- src/timetrackingexam/dal/database/dao/ProjectDBDAO.java
+++ src/timetrackingexam/dal/database/dao/ProjectDBDAO.java
@@ -123,7 +123,7 @@ public class ProjectDBDAO  implements Serializable
     
     public void getCSV(Connection con) throws FileNotFoundException
     {
-        PrintWriter pw = new PrintWriter("CSV.txt");
+        PrintWriter pw = new PrintWriter("CSV.csv");
          StringBuilder sb= new StringBuilder();
         
         String sql = "SELECT * FROM Time";
